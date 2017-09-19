1c11df038298f9356267b69718c69e09d989c2e6
Mark Burghart <mark.burghart@cerner.com>
2017-09-19 17:22:45 -0500

    started ch3 challenges

---
+++ ch3_challenges.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 17:13:41 2017
+
+@author: Mark
+"""
+
+# print 3 strings
+
+x = "sup"
+y = "hello"
+z = x + y
+print(x)
+print (y)
+print (z)
+
+print("this is another way of printing a string")
+
+
+# 2. Write program that prints a message if a variable is less than 10, and 
+#   different message if greater than or equal to 10.
+
+for i in range(15):
+    if i < 10:
+        print(i)
+        print("this value is less than 10")
+    else:
+        print(i)
+        print("this value is greater than or equal to 10")
+        
+
+# 3. write a program that prints a message if a variable is less than or equal
+#        to 10, another if the variable is greater than 10 but less than or 
+#        equal to 25, and another if the variable is greater than 25.
+        
+        
+
+# 4. Create a program that divides two variables and prints the remainder.
+        
+        
+# 5. Create a program that takes two variables, divides them, and prints the quotient
+        
+        
+# 6. Write a program with a variable 'age' assigned to integer that prints different
+#       strings depending on what integer age is.
+        
+        
+        
+#solutions: http//tinyurl.com/zx7o2v9
