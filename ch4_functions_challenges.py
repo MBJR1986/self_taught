3617e0643f50698eddaafd64d531c3d11de57d0d
Mark Burghart <mark.burghart@cerner.com>
2017-09-19 22:10:41 -0500

    added ch4 problems

---
+++ ch4_functions_challenges.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 22:03:21 2017
+
+@author: Mark Burghart
+"""
+
+# Self Taught ch4 Functions problems
+
+#1. write function that takes a numbers as an input and returns that 
+#   number squared
+    
+
+"""2. Create a function that accepts a string as a parameter and prints it"""
+
+
+"""3. Write a function that takes three required parameters and two optional parameters"""
+
+
+"""4. write a program with two functions: 1 should take an integer as a parameter
+and return the result of the integer divided by 2. 2 should take an integer as a parameter
+and return the result of the integer multiplied by 4.  Call first function, save result
+as a variable, and passit as a parameter to 2nd function.
+"""
+
+
+"""5. write a function that converts a string to a float and returns the result.
+use exception handling to catch the exception that could occur."""
+
+
+"""6. add a docstring to all of the functions you wrote in challenges 1-5."""
