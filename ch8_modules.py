092fa493b9f67dd9c9c6b87c2982ae3c3e1b0a0a
Mark Burghart <mark.burghart@cerner.com>
2017-10-16 12:42:22 -0500

    ch8 problems... short one

---
+++ ch8_modules.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 12:27:14 2017
+
+@author: Mark Burghart
+
+"""
+
+#Ch 8 : Modules
+
+"""
+1. Call a different function with the statistics module
+"""
+
+import statistics
+
+x = [1,2,3,4,5,6,7,8,9,10]
+statistics.mean(x)
+
+
